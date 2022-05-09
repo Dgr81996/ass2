1998c83487910ab4f68c48d58ac2dfafa52983ca
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-05-09 16:55:56 +0530

    Add files via upload

---
+++ 7a/src/App.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+    <input type={"password"} placeholder={"increment"}/>
+    <input type={"password"} placeholder={"decrement"}/>
+    </div>
+  );
+}
+
+export default App;
